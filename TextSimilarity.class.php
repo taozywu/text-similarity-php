cfe76c4fdb9e7ea8f84b1ae52c7169a39defb501
宋小北 <xiaobeicn@163.com>
2014-12-19 17:48:31 +0800

    add use demo

--- TextSimilarity.class.php
+++ TextSimilarity.class.php
@@ -5,8 +5,12 @@
 * 	Author:宋小北（@xiaobeicn）
 * 	
 * 	参考：
-*  		http://www.ruanyifeng.com/blog/2013/03/cosine_similarity.html 
+*  	http://www.ruanyifeng.com/blog/2013/03/cosine_similarity.html 
 *    	http://my.oschina.net/BreathL/blog/42477
+*	
+*	Use:
+*	$obj = new TextSimilarity ($text1, $text2);
+*	echo $obj->run();
 */
 
 Class TextSimilarity {
